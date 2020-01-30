a8dc19d93d313f6692a64689bd4c56f83af996d6
Amon-Ra Sanders <mrlive2k@gmail.com>
2020-01-30 04:15:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,8 @@
-def using_include(array, element)
-
+def using_include(array,element)
+ favorite_colors = ["blue", "red","purple",]
+  favorite_colors.include? ("blue")
+  favorite_colors.include? ("yellow")
+  
 end
 
 def using_sort(array)
